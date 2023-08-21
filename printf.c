9cee01e76a1f1089e5938473ca82f14ed1ab3136
Tsehafetizaz <wendetefe@gmail.com>
2023-08-21 13:09:36 -0700

    V3

--- printf.c
+++ printf.c
@@ -1,40 +1,54 @@
 #include "main.h"
-/**
- * _printf - Our custom printf function.
- * @format: The string format to print.
- * ... : The values to format and print.
- * Return: The number of characters printed.
- */
+#include <unistd.h>
+#include <stdarg.h>
+#include <string.h>
+
+int _putchar(char c)
 {
+return (write(1, &c, 1));
+}
+
+int print_char(va_list args)
+{
+return (_putchar(va_arg(args, int)));
+}
+
+int print_string(va_list args)
+{
+char *str = va_arg(args, char *);
+return (write(1, str, strlen(str)));
+}
+
 int _printf(const char *format, ...)
 {
 unsigned int i = 0, count = 0;
 va_list args;
-
+if (!format)
+{
+return (-1);
+}
 va_start(args, format);
 while (format && format[i])
 {
 if (format[i] == '%' && (format[i + 1] == 'c' || format[i + 1] == 's' || format[i + 1] == '%'))
 {
-switch (format[i + 1])
+if (format[i + 1] == 'c')
 {
-case 'c':
-count += write(1, &va_arg(args, int), 1);
-i++;
-break;
-case 's':
-// handle string here
-i++;
-break;
-case '%':
-count += write(1, &format[i], 1);
-i++;
-break;
+count += print_char(args);
 }
+else if (format[i + 1] == 's')
+{
+count += print_string(args);
+}
+else if (format[i + 1] == '%')
+{
+count += _putchar('%');
+}
+i++;
 }
 else
 {
-count += write(1, &format[i], 1);
+count += _putchar(format[i]);
 }
 i++;
 }
