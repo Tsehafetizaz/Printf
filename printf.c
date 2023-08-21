d1552f054c6484e38ff3f82f267b336364aaceb3
Tsehafetizaz <wendetefe@gmail.com>
2023-08-21 13:07:08 -0700

    first

--- printf.c
+++ printf.c
@@ -1,37 +1,41 @@
 #include "main.h"
-#include <string.h>
-
 /**
-* _printf - A basic implementation of the printf function.
-* Supports format specifiers:
-* %c: character
-* %s: string
-* %%: literal percentage character
-* @format: The format string.
-* ... : The values to format and print.
-* Return: Number of characters printed.
-*/
+ * _printf - Our custom printf function.
+ * @format: The string format to print.
+ * ... : The values to format and print.
+ * Return: The number of characters printed.
+ */
+{
 int _printf(const char *format, ...)
 {
+unsigned int i = 0, count = 0;
 va_list args;
-int i = 0, count = 0;
-char *str;
-if (!format) return (-1); 
+
 va_start(args, format);
-while (format[i])
+while (format && format[i])
 {
-if (format[i] == '%' && strchr("cs%", format[i+1]))
+if (format[i] == '%' && (format[i + 1] == 'c' || format[i + 1] == 's' || format[i + 1] == '%'))
 {
-if (format[i + 1] == 'c') count += write(1, &(char)
-{va_arg(args, int)}, 1);
-if (format[i + 1] == 's')
+switch (format[i + 1])
 {
-str = va_arg(args, char *);
-count += write(1, str ? str : "(null)", str ? strlen(str) : 6);
-}
-if (format[i + 1] == '%') count += write(1, "%", 1);
+case 'c':
+count += write(1, &va_arg(args, int), 1);
 i++;
-} else count += write(1, &format[i], 1);
+break;
+case 's':
+// handle string here
+i++;
+break;
+case '%':
+count += write(1, &format[i], 1);
+i++;
+break;
+}
+}
+else
+{
+count += write(1, &format[i], 1);
+}
 i++;
 }
 va_end(args);
