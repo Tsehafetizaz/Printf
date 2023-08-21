1d49662dace8924682e03b66bbceab66e0d6a80e
dawitalx <dawitvid@gmail.com>
2023-08-21 06:57:27 -0700

    betty-style fixed and strlen function removed

--- printf.c
+++ printf.c
@@ -1,43 +1,45 @@
 #include "main.h"
-#include <stdarg.h>
-#include <unistd.h>
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
+#include <string.h>
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
-
-	va_start(args, format);
-	for (int i = 0; format[i]; i++)
-	{
-		if (format[i] == '%' && (format[i + 1] == 'c' ||
-					format[i + 1] == 's' || format[i + 1] == '%'))
-		{
-			switch (format[i + 1])
-			{
-				case 'c':
-					count += _putchar(va_arg(args, int));
-					break;
-				case 's':
-				for (char *str = va_arg(args, char *); *str; str++)
-					count += _putchar(*str);
-				break;
-				case '%':
-				count += _putchar('%');
-				break;
-			}
-			i++;
-		}
-		else
-		{
-			count += _putchar(format[i]);
-		}
-	}
-	va_end(args);
-	return (count);
+va_list args;
+int i = 0, count = 0;
+char *str;
+if (!format)
+return (-1);
+va_start(args, format);
+while (format[i])
+{
+if (format[i] == '%' &&
+(format[i + 1] == 'c' || format[i + 1] == 's' || format[i + 1] == '%'))
+{
+switch (format[i + 1])
+{
+case 'c':
+{
+char c = va_arg(args, int);
+count += write(1, &c, 1);
+}
+i++;
+break;
+case 's':
+str = va_arg(args, char *);
+count += write(1, str, 1);
+while (*str)
+str++, count += _putchar(*str);
+break;
+case '%':
+count += write(1, "%", 1);
+i++;
+break;
+}
+}
+else
+{
+count += write(1, &format[i], 1);
+}
+i++;
+}
+va_end(args);
+return (count);
 }
