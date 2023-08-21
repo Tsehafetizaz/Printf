954c4c854f8614a819f2fc5002888ac75a2c3011
dawitalx <119086060+dawitalx@users.noreply.github.com>
2023-08-21 17:54:20 +0300

    Update printf.c

--- printf.c
+++ printf.c
@@ -5,36 +5,46 @@ int _putchar(char c) {
     return write(1, &c, 1);
 }
 
-int _printf(const char *format, ...) {
-    int count = 0;
-    int i = 0;
-    va_list args;
-    va_start(args, format);
-
-    for (i = 0; format[i]; i++) {
-        if (format[i] == '%' && (format[i + 1] == 'c' || format[i + 1] == 's' || format[i + 1] == '%')) {
-            switch (format[i + 1]) {
-                case 'c': {
-                    char c = va_arg(args, int);
-                    count += _putchar(c);
-                    break;
-                }
-                case 's': {
-                    char *str = va_arg(args, char *);
-                    while (*str)
-                        count += _putchar(*str), str++;
-                    break;
-                }
-                case '%':
-                    count += _putchar('%');
-                    break;
-            }
-            i++;
-        } else {
-            count += _putchar(format[i]);
-        }
-    }
-
-    va_end(args);
-    return count;
+int _printf(const char *format, ...)
+{
+va_list args;
+int i = 0, count = 0;
+char *str;
+if (!format)
+return (-1);
+va_start(args, format);
+while (format[i])
+{
+if (format[i] == '%' && 
+(format[i + 1] == 'c' || format[i + 1] == 's' || format[i + 1] == '%'))
+{
+switch (format[i + 1])
+{
+case 'c':
+{
+char c = va_arg(args, int);
+count += _putchar(c);
+}
+i++;
+break;
+case 's':
+str = va_arg(args, char *);
+while (*str)
+count += _putchar(*str), str++;
+i++;
+break;
+case '%':
+count += _putchar('%');
+i++;
+break;
+}
+}
+else
+{
+count += write(1, &format[i], 1);
+}
+i++;
+}
+va_end(args);
+return (count);
 }
