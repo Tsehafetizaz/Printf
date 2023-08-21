d9db1cca78698d672312002dc0edb13a802f2bcf
dawitalx <dawitvid@gmail.com>
2023-08-21 06:48:45 -0700

    code rewriten shorter

--- printf.c
+++ printf.c
@@ -1,5 +1,6 @@
 #include "main.h"
-#include <string.h>
+#include <stdarg.h>
+#include <unistd.h>
 
 int _putchar(char c)
 {
@@ -8,47 +9,35 @@ int _putchar(char c)
 
 int _printf(const char *format, ...)
 {
-va_list args;
-int i = 0, count = 0;
-char *str;
-if (!format)
-return (-1);
-va_start(args, format);
-while (format[i])
-{
-if (format[i] == '%' &&
-(format[i + 1] == 'c' || format[i + 1] == 's' || format[i + 1] == '%'))
-{
-switch (format[i + 1])
-{
-case 'c':
-{
-char c = va_arg(args, int);
-count += write(1, &c, 1);
-}
-i++;
-break;
-case 's':
-str = va_arg(args, char *);
-while (*str)
-{
-	_putchar(*str);
-	str++;
-	i++;
-}
-break;
-case '%':
-count += write(1, "%", 1);
-i++;
-break;
-}
-}
-else
-{
-count += write(1, &format[i], 1);
-}
-i++;
-}
-va_end(args);
-return (count);
+	va_list args;
+	int count = 0;
+
+	va_start(args, format);
+	for (int i = 0; format[i]; i++)
+	{
+		if (format[i] == '%' && (format[i + 1] == 'c' ||
+					format[i + 1] == 's' || format[i + 1] == '%'))
+		{
+			switch (format[i + 1])
+			{
+				case 'c':
+					count += _putchar(va_arg(args, int));
+					break;
+				case 's':
+				for (char *str = va_arg(args, char *); *str; str++)
+					count += _putchar(*str);
+				break;
+				case '%':
+				count += _putchar('%');
+				break;
+			}
+			i++;
+		}
+		else
+		{
+			count += _putchar(format[i]);
+		}
+	}
+	va_end(args);
+	return (count);
 }
