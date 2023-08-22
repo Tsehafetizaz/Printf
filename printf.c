103dfb03261fe9a5ab0488edb7c11981dadbccc5
dawitalx <dawitvid@gmail.com>
2023-08-22 02:37:06 -0700

    code updated

--- printf.c
+++ printf.c
@@ -38,6 +38,18 @@ int _printf(const char *format, ...)
 				count++;
 			}
 		}
+		else if (format[i] == '%' &&
+				(format[i + 1] == 'd' || format[i + 1] == 'i'))
+		{
+			int num = va_arg(args, int);
+			if (num < 0)
+			{
+				count += _putchar('-');
+				num = -num;
+			}
+			count += print_number(num);
+			i++;
+		}
 		else
 		{
 			_putchar(*format);
