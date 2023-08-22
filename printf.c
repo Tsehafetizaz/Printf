9cfdcec0215423392c387800665052d159b4019c
dawitalx <dawitvid@gmail.com>
2023-08-22 01:47:57 -0700

    code updated

--- printf.c
+++ printf.c
@@ -7,55 +7,44 @@
 
 int _printf(const char *format, ...)
 {
-va_list args;
-int i = 0, count = 0;
-char *str;
-if (!format)
-return (-1);
-va_start(args, format);
-while (format[i])
-{
-if (format[i] == '%' &&
-(format[i + 1] == 'c' || format[i + 1] == 's' || format[i + 1] == '%'))
-{
-switch (format[i + 1])
-{
-case 'c':
-{
-char c = va_arg(args, int);
-count += _putchar(c);
-}
-i++;
-break;
-case 's':
-str = va_arg(args, char *);
-print_string(str);
-i++;
-break;
-case '%':
-count += _putchar('%');
-i++;
-break;
-}
-}
-else if (format[i] == '%' &&
-		(format[i + 1] == 'd' || format[i + 1] == 'i'))
-{
-int num = va_arg(args, int);
-if (num < 0)
-{
-count += _putchar('-');
-num = -num;
-}
-count += print_number(num);
-i++;
-}
-else
-{
-count += write(1, &format[i], 1);
-}
-i++;
-}
-va_end(args);
-return (count);
+	va_list args;
+	int count = 0;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	while (*format != '\0')
+	{
+		if (*format == '%' && *(format + 1) != '\0')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				char c = va_arg(args, int);
+
+				count += _putchar(c);
+			}
+			else if (*format == 's')
+			{
+				const char *str = va_arg(args, char *);
+
+				count += print_string(str);
+			}
+			else
+			{
+				_putchar(*format);
+				count++;
+			}
+		}
+		else
+		{
+			_putchar(*format);
+			count++;
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
 }
