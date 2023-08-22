68481576b9dc17cede46bc1e4bb968fdb6aaaf63
dawitalx <dawitvid@gmail.com>
2023-08-22 02:33:14 -0700

    code updated

--- main.h
+++ main.h
@@ -6,8 +6,8 @@
 #include <stdlib.h>
 
 int _putchar(char c);
-int _printf(const char *format, ...);
 int print_number(int num);
 int str_len(const char *str);
 int print_string(const char *str);
+int _printf(const char *format, ...);
 #endif
