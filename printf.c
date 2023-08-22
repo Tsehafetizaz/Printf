46c9539368df8e81a4d50b9b7da9698efd900bda
Tsehafetizaz <wendetefe@gmail.com>
2023-08-22 15:10:26 -0700

    test8

---
+++ printf.c
@@ -0,0 +1,88 @@
+#include "main.h"
+/**
+ * print_char - Prints a character.
+ * @arg_list: The list of arguments.
+ * Return: Number of characters printed.
+ */
+static int print_char(va_list arg_list)
+{
+char c = (char) va_arg(arg_list, int);
+write(1, &c, 1);
+return (1);
+}
+/**
+ * print_string - Prints a string.
+ * @arg_list: The list of arguments.
+ * Return: Number of characters printed.
+ */
+static int print_string(va_list arg_list)
+{
+char *s = va_arg(arg_list, char *);
+int i = 0;
+while
+(s[i])
+{
+write(1, &s[i], 1);
+i++;
+}
+return
+(i);
+}
+/**
+ * print_percent - Prints a percent sign.
+ * @arg_list: The list of arguments.
+ * Return: Number of characters printed.
+ */
+static int print_percent(va_list arg_list)
+{
+(void) arg_list; /* unused parameter */
+char c = '%';
+write(1, &c, 1);
+return (1);
+}
+/**
+ * _printf - Our custom printf function.
+ * @format: The format string.
+ * Return: The number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+int printed_chars = 0;
+va_list arg_list;
+va_start
+(arg_list, format);
+while
+(*format)
+{
+if
+(*format == '%' && (*(format + 1) == 'c' || *(format + 1) == 's' ||
+*(format + 1) == '%'))
+(*(format + 1) == 'c' || *(format + 1) == 's' || *(format + 1) == '%'))
+{
+format++; /* skip the '%' */
+switch
+(*format)
+{
+case 'c':
+printed_chars += print_char(arg_list);
+break;
+case 's':
+printed_chars += print_string(arg_list);
+break;
+case '%':
+printed_chars += print_percent(arg_list);
+break;
+}
+}
+else
+{
+write(1, format, 1);
+printed_chars++;
+}
+format++;
+}
+va_end
+(arg_list);
+return
+(printed_chars);
+}
