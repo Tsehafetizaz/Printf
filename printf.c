c307e677459aacf59f8ed848602c33eaa73f6cd4
Tsehafetizaz <wendetefe@gmail.com>
2023-08-22 11:57:44 -0700

    alemayhehu

--- printf.c
+++ printf.c
@@ -1,67 +1,91 @@
 #include "main.h"
-
+#include <unistd.h>
+#include <stdarg.h>
+#define BUFF_SIZE 1024
+int handle_percent_case(const char *format, int i,
+char *buffer, int *buffer_index, va_list args);
 void print_buffer(char buffer[], int *buff_ind);
-
 /**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
+ * _printf - Custom printf function.
+ * @format: The format string.
+ * Return: Number of characters printed.
  */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, printed_chars = 0;
-	int flags, width, precision, size, buff_ind = 0;
-	va_list list;
-	char buffer[BUFF_SIZE];
-
-	if (format == NULL)
-		return (-1);
-
-	va_start(list, format);
-
-	for (i = 0; format && format[i] != '\0'; i++)
-	{
-		if (format[i] != '%')
-		{
-			buffer[buff_ind++] = format[i];
-			if (buff_ind == BUFF_SIZE)
-				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
-			printed_chars++;
-		}
-		else
-		{
-			print_buffer(buffer, &buff_ind);
-			flags = get_flags(format, &i);
-			width = get_width(format, &i, list);
-			precision = get_precision(format, &i, list);
-			size = get_size(format, &i);
-			++i;
-			printed = handle_print(format, &i, list, buffer,
-				flags, width, precision, size);
-			if (printed == -1)
-				return (-1);
-			printed_chars += printed;
-		}
-	}
-
-	print_buffer(buffer, &buff_ind);
-
-	va_end(list);
-
-	return (printed_chars);
+int i, printed_chars = 0, buffer_index = 0;
+va_list args;
+char buffer[BUFF_SIZE];
+if (format == NULL)
+return (-1);
+va_start(args, format);
+for (i = 0; format && format[i] != '\0'; i++)
+{
+if (format[i] != '%')
+handle_character(format[i], buffer, &buffer_index);
+else
+{
+print_buffer(buffer, &buffer_index);
+i = handle_percent_case(format, i, buffer, &buffer_index, args);
 }
-
+}
+print_buffer(buffer, &buffer_index);
+va_end(args);
+return (printed_chars);
+}
 /**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
- * @buff_ind: Index at which to add next char, represents the length.
+ * handle_character - Handles the insertion of a character into the buffer.
+ * @c: The character to insert.
+ * @buffer: The buffer.
+ * @buffer_index: The current position in the buffer.
+ */
+void handle_character(char c, char *buffer, int *buffer_index)
+{
+buffer[(*buffer_index)++] = c;
+if (*buffer_index == BUFF_SIZE)
+print_buffer(buffer, buffer_index);
+}
+/**
+ * handle_string - Handles the insertion of a string into the buffer.
+ * @str: The string to insert.
+ * @buffer: The buffer.
+ * @buffer_index: The current position in the buffer.
+ */
+void handle_string(char *str, char *buffer, int *buffer_index)
+{
+while (*str)
+{
+buffer[(*buffer_index)++] = *str++;
+if (*buffer_index == BUFF_SIZE)
+print_buffer(buffer, buffer_index);
+}
+}
+int handle_percent_case(const char *format, int i,
+char *buffer, int *buffer_index, va_list args)
+{
+i++;
+if (format[i] == 'c')
+handle_character(va_arg(args, int), buffer, buffer_index);
+else if (format[i] == 's')
+handle_string(va_arg(args, char *), buffer, buffer_index);
+else if (format[i] == '%')
+handle_character('%', buffer, buffer_index);
+else
+{
+handle_character('%', buffer, buffer_index);
+handle_character(format[i], buffer, buffer_index);
+}
+return (i);
+}
+/**
+ * print_buffer - Prints the buffer to stdout.
+ * @buffer: The buffer containing characters to print.
+ * @buff_ind: The current index in the buffer.
  */
 void print_buffer(char buffer[], int *buff_ind)
 {
-	if (*buff_ind > 0)
-		write(1, &buffer[0], *buff_ind);
-
-	*buff_ind = 0;
+if (*buff_ind > 0)
+{
+write(1, buffer, *buff_ind);
+*buff_ind = 0;
 }
-
+}
---
+++ test/put_char.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _putchar - prints character
+ * @c: char
+ * Return: write
+ */
+
+int _putchar(char c)
+{
+	write(1, &c, 1);
+	return (0);
+}
