a6c2425e072cdcf7ca056cdd688708ddd1644020
Tsehafetizaz <wendetefe@gmail.com>
2023-08-22 13:53:06 -0700

    trying again

--- main.h
+++ main.h
@@ -5,29 +5,32 @@
 #include <unistd.h>
 
 #define BUFFER_SIZE 1024
-/* struct buffer - Structure to hold a character buffer and an index */
+
+/* struct buffer: Structure to hold a character buffer and an index */
 typedef struct buffer
 {
 char buf[BUFFER_SIZE];  /* Character buffer */
 int index;              /* Current index in the buffer */
-} buffer_t;
+}
+buffer_t;
 
-/* Custom printf function */
+/* _printf: Custom printf function */
 int _printf(const char *format, ...);
 
-/* Add a character to the buffer */
+/* buffer_add: Add a character to the buffer */
 void buffer_add(buffer_t *output, char c);
 
-/* Flush the buffer, possibly sending its content to an output stream */
+/* buffer_flush: Flush the buffer, sending its content to an output stream */
 void buffer_flush(buffer_t *output);
 
-/* Print a character using variable argument lists */
+/* print_char: Print a character using variable argument lists */
 void print_char(buffer_t *output, va_list args);
 
-/* Print a string using variable argument lists */
+/* print_string: Print a string using variable argument lists */
 int print_string(buffer_t *output, va_list args);
 
-/* Print a percent sign (%) */
+/* print_percent: Print a percent sign (%) */
 void print_percent(buffer_t *output);
 
 #endif
+
