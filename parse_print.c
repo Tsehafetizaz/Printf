c019cfbc17765152f16ee5271ac4281b8a74ce49
dawitalx <119086060+dawitalx@users.noreply.github.com>
2023-08-26 01:25:36 +0300

    Update parse_print.c

--- parse_print.c
+++ parse_print.c
@@ -10,37 +10,37 @@
  */
 void parse_print(const char *format, va_list args, int *char_count)
 {
-    char ch, *str;
-    int num;
+	char ch, *str;
+	int num;
 
-    while (*format)
-    {
-        if (*format != '%')
-            print_char(*format, char_count);
-        else
-        {
-            format++;
-            if (*format == 'c')
-            {
-                ch = va_arg(args, int);
-                print_char(ch, char_count);
-            }
-            else if (*format == 's')
-            {
-                str = va_arg(args, char *);
-                if (str == NULL)
-                    print_string("(null)", char_count);
-                else
-                    print_string(str, char_count);
-            }
-            else if (*format == '%')
-                print_percent(char_count);
-            else if (*format == 'd' || *format == 'i')
-            {
-                num = va_arg(args, int);
-                _print_num(num, char_count);
-            }
-        }
-        format++;
-    }
+	while (*format)
+	{
+		if (*format != '%')
+			print_char(*format, char_count);
+		else
+		{
+			format++;
+			if (*format == 'c')
+			{
+				ch = va_arg(args, int);
+				print_char(ch, char_count);
+			}
+			else if (*format == 's')
+			{
+				str = va_arg(args, char *);
+				if (str == NULL)
+					print_string("(null)", char_count);
+				else
+					print_string(str, char_count);
+			}
+			else if (*format == '%')
+				print_percent(char_count);
+			else if (*format == 'd' || *format == 'i')
+			{
+				num = va_arg(args, int);
+				_print_num(num, char_count);
+			}
+		}
+		format++;
+	}
 }
