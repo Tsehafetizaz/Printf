047a5e8ebd128bbc00ee92e170e9e4335c0666f8
dawitalx <119086060+dawitalx@users.noreply.github.com>
2023-08-26 00:41:42 +0300

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -10,31 +10,18 @@
 */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int count = 0;
-    unsigned int i = 0;
-    if (!format) return (-1);
-    va_start(args, format);
-    while (format[i]) {
-        if (format[i] == '%' && (format[i + 1] == 'c' || format[i + 1] == 's' ||
-                                 format[i + 1] == '%')) {
-            i++;
-            switch (format[i]) {
-                case 'c':
-                    count += write(1, &(char){va_arg(args, int)}, 1);
-                    break;
-                case 's':
-                    count += write(1, va_arg(args, char*), _strlen(va_arg(args, char*)));
-                    break;
-                case '%':
-                    count += write(1, &format[i], 1);
-                    break;
-            }
-        } else {
-            count += write(1, &format[i], 1);
-        }
-        i++;
-    }
-    va_end(args);
-    return (count);
+	va_list args;
+
+	int char_count = 0;
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(args, format);
+
+	parse_and_print(format, args, &char_count);
+
+	va_end(args);
+
+	return (char_count);
 }
