0e0e8f33b1388baae236cba5374a9fdf9cd560ec
dawitalx <119086060+dawitalx@users.noreply.github.com>
2023-08-26 00:50:42 +0300

    Update main.h

--- main.h
+++ main.h
@@ -7,12 +7,13 @@
 
 int _printf(const char *format, ...);
 void parse_print(const char *format, va_list args, int *char_count);
-char *print_char(va_list args);
-char *print_string(va_list args);
-char *print_percent(void);
-char *print_integer(va_list args);
-char *print_binary(va_list args);
-char *_itoa(int num, char *str);
+char print_char(va_list args);
+char print_string(va_list args);
+void print_percent(int *char_count);
+int print_number(int num);
+//char *print_integer(va_list args);
+//char *print_binary(va_list args);
+//char *_itoa(int num, char *str);
 int _putchar(char c);
 int _strlen(const char *s);
 
