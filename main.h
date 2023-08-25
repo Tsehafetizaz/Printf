5811496acef44ad48b72686c0b27443f3cb6b33a
dawitalx <119086060+dawitalx@users.noreply.github.com>
2023-08-26 00:44:40 +0300

    Update main.h

--- main.h
+++ main.h
@@ -6,6 +6,7 @@
 #include <unistd.h>
 
 int _printf(const char *format, ...);
+void parse_print(const char *format, va_list args, int *char_count);
 char *print_char(va_list args);
 char *print_string(va_list args);
 char *print_percent(void);
