665d31981eb3cde90a34117b492a9785ef8edba5
dawitalx <119086060+dawitalx@users.noreply.github.com>
2023-08-27 12:55:48 +0300

    Betty-style

--- functions.c
+++ functions.c
@@ -1,7 +1,8 @@
 #include <unistd.h>
 
-int _putchar(char c) {
-    return write(1, &c, 1);
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
 }
 
 void print_string(char *str)
@@ -21,31 +22,33 @@ void print_string(char *str)
  */
 char *print_number(int num)
 {
-    static char numi[1024];
-
-    int divisor = 1;
-    char *result = numi;
-
-    if (num < 0) {
-        *result = '-';
-        result++;
-        num = -num;
-    }
-
-    while (num / divisor >= 10) {
-        divisor *= 10;
-    }
-
-    while (divisor > 0) {
-        int digit = num / divisor;
-        *result = '0' + digit;
-        result++;
-        num %= divisor;
-        divisor /= 10;
-    }
-
-    *result = '\0';
-    return (numi);
+	static char numi[1024];
+	
+	int divisor = 1;
+	char *result = numi;
+	
+	if (num < 0)
+	{
+		*result = '-';
+		result++;
+		num = -num;
+	}
+	
+	while (num / divisor >= 10)
+	{
+		divisor *= 10;
+	}
+	
+	while (divisor > 0) {
+		int digit = num / divisor;
+		*result = '0' + digit;
+		result++;
+		num %= divisor;
+		divisor /= 10;
+	}
+	
+	*result = '\0';
+	return (numi);
 }
 
 char *print_binary(int num)
